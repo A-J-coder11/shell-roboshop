42bddb3da555c73ce7d4815263a5a7ee80958a0d
A-J-coder11 <anand.coder.j@gmail.com>
2025-07-14 16:18:10 +0530

    roboshop

---
+++ roboshop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-09b7c101d210b7538"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z0118759218EWKCQN2MBA"
+DOMAIN-NAME="twous.sbs"
+
+for instance in ${INSTANCES[@]}
+do 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f  --instance-type  t2.micro --security-group-ids  sg-09b7c101d210b7538 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=test}]" --query "Instances[0].InstanceId" --output text)
+  if [ $instance != "frontend" ]
+  then 
+     Ip=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text
+  else
+     Ip=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output te
+  fi
+  echo "$instance Ip adsress: $IP"
+done
