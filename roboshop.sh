d70695bfdc7bdd60e649150308f6a1077ab99c51
A-J-coder11 <anand.coder.j@gmail.com>
2025-07-14 16:48:36 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -4,11 +4,11 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-09b7c101d210b7538"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z0118759218EWKCQN2MBA"
-DOMAIN-NAME="twous.sbs"
+DOMAIN_NAME="twous.sbs"
 
 for instance in ${INSTANCES[@]}
 do 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f  --instance-type  t2.micro --security-group-ids  sg-09b7c101d210b7538 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f  --instance-type  t2.micro --security-group-ids  sg-09b7c101d210b7538 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
   if [ $instance != "frontend" ]
   then 
      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
