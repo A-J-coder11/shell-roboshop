e712775db3a62446b3c4d5455a916e280c66344b
A-J-coder11 <anand.coder.j@gmail.com>
2025-07-16 22:06:54 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -32,7 +32,7 @@ VALIDATE(){
   fi
 }
 
-cp mongodb.repo c/etc/yum.repos.d/mongo.repo
+cp mongo.repo c/etc/yum.repos.d/mongo.repo
 VALIDATE $? "copying MongoDB repo"
 
 dnf install mongodb-org -y &>>$LOG_FILE
