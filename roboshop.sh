21a0e995fa549b7d89f4051d40a24a1f9597731e
A-J-coder11 <anand.coder.j@gmail.com>
2025-07-16 14:05:26 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -8,12 +8,31 @@ DOMAIN_NAME="twous.sbs"
 
 for instance in ${INSTANCES[@]}
 do 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f  --instance-type  t2.micro --security-group-ids  sg-09b7c101d210b7538 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f  --instance-type  t3.micro --security-group-ids  sg-09b7c101d210b7538 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
   if [ $instance != "frontend" ]
   then 
      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
   else
      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
   fi
-  echo "$instance IP adsress: $IP"
+  echo "$instance IP address: $IP"
+
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $zoneid \
+  --change-batch '
+  {
+    "Comment": "Creating or updating a record set for cognito endpoint"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$instance'.'$DOMAIN_NAME'"
+        ,"Type"             : "CNAME"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+
 done
