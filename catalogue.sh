4be407c8c3d25f0b98c4cbf16054edaab14a9e6b
A-J-coder11 <anand.coder.j@gmail.com>
2025-07-17 21:01:41 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -77,7 +77,13 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "installing mongodb client"
 
-mongosh --host mongodb.twous.sbs </app/db/master-data.js &>>$LOG_FILE
-VALIDATE $? "Loading data into MongoDB"
 
+STATUS=(mongosh --host mongodb.twous.sbs --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if ($STATUS -lt 0)
+then
+    mongosh --host mongodb.twous.sbs </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading data into MongoDB"
+else
+  echo -e "Data is already loaded...$Y Skipping $N"
 
+fi
